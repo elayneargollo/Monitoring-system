9a14b7b5b0e8362a4f9566b7dc7213ff65497800
elayneargollo <elayne.natalia@outlook.com>
2020-10-11 14:20:26 -0300

    Fatorando menu

--- src/ifba/ads/view/MonitorDeUnidade.java
+++ src/ifba/ads/view/MonitorDeUnidade.java
@@ -20,28 +20,26 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
 
 @SuppressWarnings("serial")
 public class MonitorDeUnidade extends JFrame {
 
-	private JTextField txtLatitude;
-	private JTextField txtLongitude;
+	private JTextField latitude;
+	private JTextField longitude;
 	private Monitorar areaMonitorada;
 	private JCheckBox cameraDeVideo;
 	private JCheckBox medidorCO2;
 	private JCheckBox medidorMetano;
 	private JCheckBox termometro;
-
+	private JPanel start;
+	private static MenuBar menuBar;
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					MonitorDeUnidade frame = new MonitorDeUnidade();
-					frame.menu();
-					frame.setVisible(true);
+					menuBar = new MenuBar(frame);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -56,16 +54,16 @@ public class MonitorDeUnidade extends JFrame {
 
 		setTitle("Monitoramento de Unidade Móvel");
 
-		JPanel painel = new JPanel();
-		painel.setPreferredSize(new Dimension(500, 360));
-		getContentPane().add(painel, BorderLayout.CENTER);
-		painel.setLayout(null);
+		start = new JPanel();
+		start.setPreferredSize(new Dimension(500, 360));
+		getContentPane().add(start, BorderLayout.CENTER);
+		start.setLayout(null);
 
 		JPanel equipamento = new JPanel();
 		equipamento.setBorder(new TitledBorder(new LineBorder(new Color(184, 207, 229)), "Equipamentos",
 				TitledBorder.LEADING, TitledBorder.TOP, null, new Color(51, 51, 51)));
 		equipamento.setBounds(23, 137, 213, 139);
-		painel.add(equipamento);
+		start.add(equipamento);
 		equipamento.setLayout(null);
 
 		cameraDeVideo = new JCheckBox("Câmera de Vídeo");
@@ -88,31 +86,31 @@ public class MonitorDeUnidade extends JFrame {
 		localizacao.setBorder(new TitledBorder(new LineBorder(new Color(184, 207, 229)), "Localiza\u00E7\u00E3o",
 				TitledBorder.LEADING, TitledBorder.TOP, null, new Color(51, 51, 51)));
 		localizacao.setBounds(23, 28, 213, 87);
-		painel.add(localizacao);
+		start.add(localizacao);
 		localizacao.setLayout(null);
 
 		JLabel lblLatitude = new JLabel("Latitude");
 		lblLatitude.setBounds(10, 24, 60, 15);
 		localizacao.add(lblLatitude);
 
-		txtLatitude = new JTextField();
-		txtLatitude.setBounds(75, 22, 114, 19);
+		latitude = new JTextField();
+		latitude.setBounds(75, 22, 114, 19);
 		;
-		localizacao.add(txtLatitude);
-		txtLatitude.setColumns(10);
+		localizacao.add(latitude);
+		latitude.setColumns(10);
 
 		JLabel lblLongitude = new JLabel("Longitude");
 		lblLongitude.setBounds(10, 51, 79, 15);
 		localizacao.add(lblLongitude);
 
-		txtLongitude = new JTextField();
-		txtLongitude.setBounds(97, 49, 92, 19);
-		localizacao.add(txtLongitude);
-		txtLongitude.setColumns(10);
+		longitude = new JTextField();
+		longitude.setBounds(97, 49, 92, 19);
+		localizacao.add(longitude);
+		longitude.setColumns(10);
 
 		JButton btnMover = new JButton("Mover");
 		btnMover.setBounds(23, 296, 213, 25);
-		painel.add(btnMover);
+		start.add(btnMover);
 		pack();
 		setLocationRelativeTo(null);
 
@@ -130,49 +128,27 @@ public class MonitorDeUnidade extends JFrame {
 	}
 
 	public boolean getCamera() {
-		return cameraDeVideo.isSelected();
+		return this.cameraDeVideo.isSelected();
 	}
 
 	public boolean getMedidorC02() {
-		return medidorCO2.isSelected();
+		return this.medidorCO2.isSelected();
 	}
 
 	public boolean getTermomentro() {
-		return termometro.isSelected();
+		return this.termometro.isSelected();
 	}
 
 	public boolean getMedidorMetano() {
-		return medidorMetano.isSelected();
+		return this.medidorMetano.isSelected();
 	}
 
 	public float getLongitude() {
-		return Float.parseFloat(txtLongitude.getText());
+		return Float.parseFloat(this.longitude.getText());
 	}
 
 	public float getLatitude() {
-		return Float.parseFloat(txtLatitude.getText());
-	}
-
-	public void menu() {
-
-		JMenuBar menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
-
-		JMenu mnUnidades = new JMenu("Unidades");
-		menuBar.add(mnUnidades);
-
-		JMenuItem mntmBuscar = new JMenuItem("Buscar");
-		mnUnidades.add(mntmBuscar);
-
-		JMenu mnSobre = new JMenu("Sobre");
-		menuBar.add(mnSobre);
-
-		JMenuItem mntmVerso = new JMenuItem("Versão");
-		mnSobre.add(mntmVerso);
-
-		JMenuItem mntmAjuda = new JMenuItem("Ajuda");
-		mnSobre.add(mntmAjuda);
-
+		return Float.parseFloat(this.latitude.getText());
 	}
 
 }
