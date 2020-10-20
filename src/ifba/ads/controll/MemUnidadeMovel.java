a4324f50d867fc62adc4d52163468d7043979107
elayneargollo <elayne.natalia@outlook.com>
2020-10-20 02:20:05 -0300

    Corrigindo map e adicionando equipamentos

--- src/ifba/ads/controll/MemUnidadeMovel.java
+++ src/ifba/ads/controll/MemUnidadeMovel.java
@@ -2,66 +2,71 @@ package ifba.ads.controll;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import ifba.ads.model.ConfiguracaoDaUnidade;
+import ifba.ads.model.EquipamentoEnum;
 import ifba.ads.model.UnidadeEuclidiana;
 import ifba.ads.model.UnidadeManhattan;
 import ifba.ads.model.UnidadeMovel;
 
 public class MemUnidadeMovel implements UnidadeMovelDAO {
-	
-	private Map<String, UnidadeMovel> unidades;
-	
+
+	private ArrayList<UnidadeMovel> unidades = new ArrayList();
+
 	public MemUnidadeMovel() throws Exception {
 		try {
-			
 			this.povoar();
-				} catch (SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	  private void povoar() throws Exception {
-			
-		  	ConfiguracaoDaUnidade configuracao = new ConfiguracaoDaUnidade();
-			ConfiguracaoDaUnidade configuracao2 = new ConfiguracaoDaUnidade();
-			
-			this.inserir(new UnidadeEuclidiana("teste", 4, 2, configuracao));
-			this.inserir(new UnidadeManhattan("teste2", 42, 24, configuracao2));
-		  	this.inserir(new UnidadeEuclidiana("teste3", 3, 2, configuracao));
-			
-	    }
-	
+
+	private void povoar() throws Exception {
+
+		ConfiguracaoDaUnidade configuracao = new ConfiguracaoDaUnidade();
+		ConfiguracaoDaUnidade configuracao2 = new ConfiguracaoDaUnidade();
+		
+		configuracao.adicionarEquipamentosAUnidade(EquipamentoEnum.CAMERADEVIDEO);
+		configuracao.adicionarEquipamentosAUnidade(EquipamentoEnum.MEDIDORDEC02);
+		configuracao2.adicionarEquipamentosAUnidade(EquipamentoEnum.MEDIDORDEC02);
+		
+		this.inserir(new UnidadeEuclidiana("teste", 4, 2, configuracao));
+		this.inserir(new UnidadeManhattan("teste2", 42, 24, configuracao2));
+		this.inserir(new UnidadeEuclidiana("teste3", 3, 2, configuracao));
+
+	}
+
 	@Override
 	public void inserir(UnidadeMovel unidade) {
-		this.unidades.put(unidade.getId(), unidade);		
+		unidades.add(unidade);
 	}
 
 	@Override
 	public void atualizar(UnidadeMovel unidade) {
-		this.unidades.put(unidade.getId(), unidade);
-		
+		this.unidades.add(unidade);
+
 	}
 
 	@Override
 	public int deletar(String id) {
 		this.unidades.remove(id);
-		
 		return 0;
 	}
 
 	@Override
 	public ArrayList<UnidadeMovel> buscarTodasAsUnidades() {
-		
-		return null;
+		return unidades;
 	}
 
 	@Override
-	public UnidadeMovel buscarPorId(String id) {
-		return this.unidades.get(id);
+	public UnidadeMovel buscarPorId(String id)  {
+		
+		for (int i=0; i<unidades.size(); i++) {
+			if(unidades.get(i).getId().equals(id)) {
+				return unidades.get(i);
+			}
+		}
+		return null;
 	}
-	
+
 }
--- src/ifba/ads/model/AppTeste.java
+++ src/ifba/ads/model/AppTeste.java
@@ -1,33 +1,17 @@
 package ifba.ads.model;
 
-import java.sql.SQLException;
-
-import ifba.ads.controll.H2unidadeMovel;
+import ifba.ads.controll.MemUnidadeMovel;
 
 public class AppTeste {
 
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) throws Exception {
 		
-		/* Configuracao da camada de negocio */
-
-		ConfiguracaoDaUnidade configuracao = new ConfiguracaoDaUnidade();
-		ConfiguracaoDaUnidade configuracao2 = new ConfiguracaoDaUnidade();
-		ConfiguracaoDaUnidade configuracao3 = new ConfiguracaoDaUnidade();
-
-		configuracao.adicionarEquipamentosAUnidade(EquipamentoEnum.CAMERADEVIDEO);
-		configuracao.adicionarEquipamentosAUnidade(EquipamentoEnum.MEDIDORDEC02);
-		configuracao2.adicionarEquipamentosAUnidade(EquipamentoEnum.MEDIDORDEC02);
-		configuracao3.adicionarEquipamentosAUnidade(EquipamentoEnum.CAMERADEVIDEO);
-
-		UnidadeMovel unidade = new UnidadeEuclidiana("teste", 4, 2, configuracao);
-		UnidadeMovel unidade3 = new UnidadeEuclidiana("teste3", 3, 2, configuracao);
-		UnidadeMovel unidadeM = new UnidadeManhattan("teste2", 42, 24, configuracao2);
-
-		Monitorar areaMonitorada = new Monitorar();
-	//	areaMonitorada.unidadeMovelDAO.buscarUnidadesH2();
-
-		System.out.println("=> "+areaMonitorada.unidadeMovelMaisProxima(7, 5, true, false, true, false));
+		MemUnidadeMovel me = new MemUnidadeMovel();
+		
+		System.out.println(me.buscarTodasAsUnidades());
+		System.out.println(me.buscarPorId("teste"));
 
 	}
+	
 
 }
